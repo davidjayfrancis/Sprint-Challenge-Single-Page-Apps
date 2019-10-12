f892b6e068c62a85654609f99dbcafb59ba2f6a3
David Francis <davidjayfrancis@gmail.com>
2019-10-12 10:36:43 -0700

    added routes and links to WelcomePage and CharacterList component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Link to="/">Welcome</Link>
+      <Link to="characters">Characters</Link>
     </header>
   );
 }
