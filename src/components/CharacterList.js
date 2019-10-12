5db60553de618a4c6747221d481fef6994de77a5
David Francis <davidjayfrancis@gmail.com>
2019-10-12 12:07:17 -0700

    added a form with a search box that filters the displayed characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard.js";
 import styled from "styled-components";
+import SearchForm from "./SearchForm.js";
 
 const Container = styled.div`
   display: flex;
@@ -16,7 +17,11 @@ const Title = styled.h3`
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  // people will get filtered based on the form
+  //initialstate will hold the full list of returned objects
   const [people, setPeople] = useState([]);
+  const [initialState, setInitialState] = useState([]);
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
@@ -25,8 +30,8 @@ export default function CharacterList() {
         const peopleList = await axios.get(
           "https://rickandmortyapi.com/api/character/"
         );
-        console.log(peopleList);
         setPeople(peopleList.data.results);
+        setInitialState(peopleList.data.results);
       } catch (err) {
         console.log(`Error! ${err}`);
       } //end of try/catch
@@ -34,8 +39,15 @@ export default function CharacterList() {
     fetchData();
   }, []);
 
+  const filterCharacters = query => {
+    // need initial state so we can reset when queries are deleted/backspaced
+    setPeople(initialState);
+    setPeople(initialState.filter(x => x["name"].includes(query)));
+  };
+
   return (
     <div>
+      <SearchForm searchFn={filterCharacters} />
       <section className="character-list">
         <Title>Ricky and Morty Characters</Title>
         <Container>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+  const handleChange = e => {
+    //somehow needs to filter displayed data...
+    // for each item in the array, see if it contains the search term
+    // and display it if it does
+    props.searchFn(e.target.value);
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <label htmlFor="search">Search</label>
+        <input
+          type="text"
+          name="search"
+          id="searchFilter"
+          onChange={handleChange}
+        ></input>
+      </form>
     </section>
   );
 }
