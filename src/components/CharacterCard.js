b207f5188fe24f177c7ded1123a845f92856b1f4
David Francis <davidjayfrancis@gmail.com>
2019-10-12 11:09:46 -0700

    adding styling with styled-components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,20 @@
 import React from "react";
 import CharacterList from "./CharacterList.js";
-// import jssPluginPropsSort from "jss-plugin-props-sort";
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 300px;
+  margin: 10px;
+  background: rgba(100, 40, 50, 0.7);
+  color: rgb(240, 220, 240);
+  border-radius: 10px;
+`;
 
 export default function CharacterCard(props) {
   return (
-    <div>
+    <Card>
       <h3>Name: {props.name}</h3>
       <p>Species: {props.species}</p>
-    </div>
+    </Card>
   );
 }
